15c2462a10809f560212ea5e8c29fc36586bc713
Nathan Broadbent <nathan.f77@gmail.com>
2013-07-13 23:22:29 +1200

    Fix http generated images path when using Compass sprites
    Otherwise compass uses '/source/images' for the sprite image URL

--- config.rb
+++ config.rb
@@ -3,11 +3,11 @@ require 'sass-globbing'
 # Require any additional compass plugins here.
 project_type = :stand_alone
 
-# Publishing paths
-http_path = "/flavio/"
-http_images_path = "/flavio/images"
-http_fonts_path = "/flavio/fonts"
-css_dir = "public/flavio/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
